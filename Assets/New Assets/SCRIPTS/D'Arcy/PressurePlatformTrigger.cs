3c45825bc818f853b0da47772bbfe8b9550257ce
D'Arcy Sullivan <d_sullivan@hotmail.co.uk>
2018-04-10 00:55:57 +0100

    Moar doors
    Level 1 is semi-functional
    Added collectable requirment doors and moving platforms between 2 points (will probably be changed).

---
+++ Assets/New Assets/SCRIPTS/D'Arcy/PressurePlatformTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlatformTrigger : MonoBehaviour
+{
+    public GameObject _controller;
+
+    private void OnTriggerStay(Collider other)
+    {
+        _controller.GetComponent<PressurePlatfromController>().move = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _controller.GetComponent<PressurePlatfromController>().move = false;
+    }
+}
---
+++ Assets/New Assets/SCRIPTS/D'Arcy/PressurePlatfromController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlatfromController : MonoBehaviour
+{
+    public GameObject _plate;
+    public GameObject _platform;
+    public GameObject _pos_0;
+    public GameObject _pos_1;
+    public bool move = false;
+    private bool flip = false;
+    public Material _mat_active;
+
+    private Material _mat_inactive;
+
+    // Use this for initialization
+    void Start()
+    {
+        _mat_inactive = _plate.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (move)
+        {
+            if(_platform.transform.position.x >= _pos_0.transform.position.x - 1)
+            {
+                flip = false;
+            }
+            if (_platform.transform.position.x <= _pos_1.transform.position.x + 1)
+            {
+                flip = true;
+            }
+            if(flip)
+            {
+                _platform.transform.position = Vector3.Lerp(_platform.transform.position, _pos_0.transform.position, Time.deltaTime / 2);
+            }
+            else
+            {
+                _platform.transform.position = Vector3.Lerp(_platform.transform.position, _pos_1.transform.position, Time.deltaTime / 2);
+            }
+            _plate.GetComponent<Renderer>().material = _mat_active;
+
+        }
+        else
+        {
+            _plate.GetComponent<Renderer>().material = _mat_inactive;
+        }
+    }
+}
