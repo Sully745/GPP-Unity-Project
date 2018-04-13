c410c0c025e30275e8d92e1466ab67606c3bf668
Luke Roberts <luke4.roberts@live.uwe.ac.uk>
2018-04-13 23:24:38 +0100

    added smoke

--- Assets/New Assets/SCRIPTS/Luke/S_Flames.cs
+++ Assets/New Assets/SCRIPTS/Luke/S_Flames.cs
@@ -4,54 +4,113 @@ using UnityEngine;
 
 public class S_Flames : MonoBehaviour
 {
-    public Transform LeftFoot;
-    public Transform RightFoot;
-    public ParticleSystem Flames;
+    [SerializeField]
+    private Transform LeftFoot;
+    [SerializeField]
+    private Transform RightFoot;
+    [SerializeField]
+    private float DurationInSeconds = 10;
+    [SerializeField]
+    private bool FlamesTest = false;
+    [SerializeField]
+    private bool SmokeTest = false;
+    [SerializeField]
+    private ParticleSystem FlamesPrefab;
+    [SerializeField]
+    private ParticleSystem SmokePrefab;
 
     private Quaternion up;
-    private ParticleSystem left;
-    private ParticleSystem right;
 
-    // Use this for initialization
-    void Start ()
+    private ParticleSystem LeftFlames;
+    private ParticleSystem RightFlames;
+
+    private ParticleSystem LeftSmoke;
+    private ParticleSystem RightSmoke;
+
+    void Start()
     {
         up = Quaternion.Euler(0.0f, 90.0f, 0.0f);
 
-        StartCoroutine(_CreateFeetFlames(10.0f));
+        LeftFlames = Instantiate(FlamesPrefab, LeftFoot.position, up);
+        LeftFlames.transform.SetParent(LeftFoot);
+        LeftFlames.transform.localRotation = up;
+        LeftFlames.Pause();
+
+        RightFlames = Instantiate(FlamesPrefab, RightFoot.position, up);
+        RightFlames.transform.SetParent(RightFoot);
+        RightFlames.transform.localRotation = up;
+        RightFlames.Pause();
+
+        LeftSmoke = Instantiate(SmokePrefab, LeftFoot.position, up);
+        LeftSmoke.transform.SetParent(LeftFoot);
+        LeftSmoke.transform.localRotation = up;
+        LeftSmoke.Pause();
+
+        RightSmoke = Instantiate(SmokePrefab, RightFoot.position, up);
+        RightSmoke.transform.SetParent(RightFoot);
+        RightSmoke.transform.localRotation = up;
+        RightSmoke.Pause();
     }
 
     private void Update()
     {
-        left.transform.localRotation = up;
-        right.transform.localRotation = up;
+        if (FlamesTest)
+        {
+            StartCoroutine(_PlayFlames(DurationInSeconds));
+            FlamesTest = false;
+        }
+        if (SmokeTest)
+        {
+            StartCoroutine(_PlaySmoke(DurationInSeconds));
+            SmokeTest = false;
+        }
     }
 
-    public IEnumerator _CreateFeetFlames(float duration)
+    public IEnumerator _PlayFlames(float duration)
     {
-        if (!left)
+        if (!LeftFlames.isPlaying)
         {
-            Debug.Log("Spawning Left Fire");
-            left = Instantiate(Flames, LeftFoot.position, up);
-            left.transform.SetParent(LeftFoot);
+            LeftFlames.Play(true);
         }
-        if (!right)
+        if (!RightFlames.isPlaying)
         {
-            Debug.Log("Spawning Right Fire");
-            right = Instantiate(Flames, RightFoot.position, up);
-            right.transform.SetParent(RightFoot);
+            RightFlames.Play(true);
         }
 
         yield return new WaitForSeconds(duration);
-        
-        if (left)
+
+        if (LeftFlames.isPlaying)
         {
-            Debug.Log("Killing Left Fire");
-            Destroy(left.gameObject);
+            LeftFlames.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
-        if (right)
+        if (RightFlames.isPlaying)
         {
-            Debug.Log("Killing Right Fire");
-            Destroy(right.gameObject);
+            RightFlames.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+    }
+
+    public IEnumerator _PlaySmoke(float duration)
+    {
+        duration = 1.5f;
+
+        if (!LeftSmoke.isPlaying)
+        {
+            LeftSmoke.Play(true);
+        }
+        if (!RightSmoke.isPlaying)
+        {
+            RightSmoke.Play(true);
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        if (LeftSmoke.isPlaying)
+        {
+            LeftSmoke.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+        if (RightSmoke.isPlaying)
+        {
+            RightSmoke.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
     }
 }
