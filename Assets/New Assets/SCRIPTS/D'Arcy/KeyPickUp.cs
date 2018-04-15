a7030f056b6e887b38ba756d278ef1445c7e178a
D'Arcy Sullivan <31903363+Sully745@users.noreply.github.com>
2018-04-15 13:46:58 +0100

    Add files via upload

--- Assets/New Assets/SCRIPTS/D'Arcy/KeyPickUp.cs
+++ Assets/New Assets/SCRIPTS/D'Arcy/KeyPickUp.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class KeyPickUp : MonoBehaviour
 {
+
+	private void Update()
+	{
+		transform.Rotate(0, 40 * Time.deltaTime * 2 , 0);
+	}
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
