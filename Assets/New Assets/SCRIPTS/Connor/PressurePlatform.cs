4ad436d1e835837d7aae5765e19f09225ec3cf26
D'Arcy Sullivan <31903363+Sully745@users.noreply.github.com>
2018-04-18 13:47:55 +0100

    Merge of new and Master

---
+++ Assets/New Assets/SCRIPTS/Connor/PressurePlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlatform : MonoBehaviour {
+
+    public GameObject platform;
+    public Material mat_off;
+    public Material mat_on;
+	// Use this for initialization
+	void Start () {
+
+        platform.GetComponent<ObjectPathFollow>().speed = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerStay (Collider coll)
+    {
+        platform.GetComponent<ObjectPathFollow>().speed = 5;
+        GetComponent<MeshRenderer>().material = mat_on;
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        platform.GetComponent<ObjectPathFollow>().speed = 0;
+        GetComponent<MeshRenderer>().material = mat_off;
+    }
+}
