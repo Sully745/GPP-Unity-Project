0e8e8ad4407ac91e3e6346a1a09628da41217b03
Luke Roberts <luke4.roberts@live.uwe.ac.uk>
2018-04-20 11:19:49 +0100

    lovely firewroks

--- Assets/New Assets/SCRIPTS/Luke/S_Flames.cs
+++ Assets/New Assets/SCRIPTS/Luke/S_Flames.cs
@@ -5,51 +5,33 @@ using UnityEngine;
 public class S_Flames : MonoBehaviour
 {
     [SerializeField]
-    private Transform LeftFoot;
+    private Transform[] Positions;
     [SerializeField]
-    private Transform RightFoot;
+    private ParticleSystem[] Flames;
     [SerializeField]
     private float DurationInSeconds = 10;
     [SerializeField]
     private bool FlamesTest = false;
     [SerializeField]
-    private bool SmokeTest = false;
-    [SerializeField]
     private ParticleSystem FlamesPrefab;
-    [SerializeField]
-    private ParticleSystem SmokePrefab;
-
+    
     private Quaternion up;
 
-    private ParticleSystem LeftFlames;
-    private ParticleSystem RightFlames;
-
-    private ParticleSystem LeftSmoke;
-    private ParticleSystem RightSmoke;
-
-    void Start()
+    void Start ()
     {
         up = Quaternion.Euler(0.0f, 90.0f, 0.0f);
 
-        LeftFlames = Instantiate(FlamesPrefab, LeftFoot.position, up);
-        LeftFlames.transform.SetParent(LeftFoot);
-        LeftFlames.transform.localRotation = up;
-        LeftFlames.Pause();
+        Flames = new ParticleSystem[Positions.Length];
 
-        RightFlames = Instantiate(FlamesPrefab, RightFoot.position, up);
-        RightFlames.transform.SetParent(RightFoot);
-        RightFlames.transform.localRotation = up;
-        RightFlames.Pause();
-
-        LeftSmoke = Instantiate(SmokePrefab, LeftFoot.position, up);
-        LeftSmoke.transform.SetParent(LeftFoot);
-        LeftSmoke.transform.localRotation = up;
-        LeftSmoke.Pause();
-
-        RightSmoke = Instantiate(SmokePrefab, RightFoot.position, up);
-        RightSmoke.transform.SetParent(RightFoot);
-        RightSmoke.transform.localRotation = up;
-        RightSmoke.Pause();
+        int i = 0;
+        foreach (Transform position in Positions)
+        {
+            Flames[i] = Instantiate(FlamesPrefab, position.position, up);
+            Flames[i].transform.SetParent(position);
+            Flames[i].transform.localRotation = up;
+            Flames[i].Pause();
+            i++;
+        }
     }
 
     private void Update()
@@ -59,58 +41,26 @@ public class S_Flames : MonoBehaviour
             StartCoroutine(_PlayFlames(DurationInSeconds));
             FlamesTest = false;
         }
-        if (SmokeTest)
-        {
-            StartCoroutine(_PlaySmoke(DurationInSeconds));
-            SmokeTest = false;
-        }
     }
 
     public IEnumerator _PlayFlames(float duration)
     {
-        if (!LeftFlames.isPlaying)
+        foreach (ParticleSystem Flame in Flames)
         {
-            LeftFlames.Play(true);
-        }
-        if (!RightFlames.isPlaying)
-        {
-            RightFlames.Play(true);
-        }
-
-        yield return new WaitForSeconds(duration);
-
-        if (LeftFlames.isPlaying)
-        {
-            LeftFlames.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-        }
-        if (RightFlames.isPlaying)
-        {
-            RightFlames.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-        }
-    }
-
-    public IEnumerator _PlaySmoke(float duration)
-    {
-        duration = 1.5f;
-
-        if (!LeftSmoke.isPlaying)
-        {
-            LeftSmoke.Play(true);
-        }
-        if (!RightSmoke.isPlaying)
-        {
-            RightSmoke.Play(true);
+            if (!Flame.isPlaying)
+            {
+                Flame.Play(true);
+            }
         }
 
         yield return new WaitForSeconds(duration);
 
-        if (LeftSmoke.isPlaying)
+        foreach (ParticleSystem Flame in Flames)
         {
-            LeftSmoke.Stop(true, ParticleSystemStopBehavior.StopEmitting);
-        }
-        if (RightSmoke.isPlaying)
-        {
-            RightSmoke.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            if (Flame.isPlaying)
+            {
+                Flame.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            }
         }
     }
 }
---
+++ Assets/New Assets/SCRIPTS/Luke/S_Lightning.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Lightning : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Positions;
+    [SerializeField]
+    private ParticleSystem[] Lightning;
+    [SerializeField]
+    private float DurationInSeconds = 10;
+    [SerializeField]
+    private bool LightningTest = false;
+    [SerializeField]
+    private ParticleSystem LightningPrefab;
+
+    private Quaternion up;
+
+    void Start()
+    {
+        up = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+
+        Lightning = new ParticleSystem[Positions.Length];
+
+        int i = 0;
+        foreach (Transform position in Positions)
+        {
+            Lightning[i] = Instantiate(LightningPrefab, position.position, up);
+            Lightning[i].transform.SetParent(position);
+            Lightning[i].transform.localRotation = up;
+            Lightning[i].Pause();
+            i++;
+        }
+    }
+
+    private void Update()
+    {
+        if (LightningTest)
+        {
+            StartCoroutine(_PlayFlames(DurationInSeconds));
+            LightningTest = false;
+        }
+    }
+
+    public IEnumerator _PlayFlames(float duration)
+    {
+        foreach (ParticleSystem l in Lightning)
+        {
+            if (!l.isPlaying)
+            {
+                l.Play(true);
+            }
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        foreach (ParticleSystem l in Lightning)
+        {
+            if (l.isPlaying)
+            {
+                l.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            }
+        }
+    }
+}
