5bfcfdbbc96267e24ca732ce861052a1bc3c243b
Connor-King <32358172+Connor-King@users.noreply.github.com>
2018-04-27 14:17:21 +0100

    Final

---
+++ Assets/EFFECTS/SCRIPTS/S_ColourGradient.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ColourGradient : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem[] particleSystems;
+    [SerializeField]
+    private bool randomColours = true;
+    private Gradient gradient;
+    private GradientColorKey[] gck;
+    private GradientAlphaKey[] gak;
+    private bool waiting = false;
+
+    void Update()
+    {
+        if (randomColours && !waiting)
+        {
+            StartCoroutine(_newColours());
+        }
+    }
+
+    IEnumerator _newColours()
+    {
+        waiting = true;
+        foreach (ParticleSystem ps in particleSystems)
+        {
+            var col = ps.colorOverLifetime;
+            col.color = NewGradient();          
+        }
+        yield return new WaitForSeconds(5.0F);
+        waiting = false;
+    }
+
+    Gradient NewGradient()
+    {
+        gradient = new Gradient();
+
+        gck = new GradientColorKey[2];
+        gck[0].color = new Color(rn(), rn(), rn());
+        gck[0].time = 0.33F;
+        gck[1].color = new Color(rn(), rn(), rn());
+        gck[1].time = 0.66F;
+
+        gak = new GradientAlphaKey[3];
+        gak[0].alpha = 0.0F;
+        gak[0].time = 0.0F;
+        gak[1].alpha = 1.0F;
+        gak[1].time = 0.50F;
+        gak[2].alpha = 0.0F;
+        gak[2].time = 1.0F;
+
+        gradient.SetKeys(gck, gak);
+
+        return gradient;
+    }
+
+    float rn()
+    {
+        return Random.Range(0.0F, 1.0F);
+    }
+}
---
+++ Assets/EFFECTS/SCRIPTS/S_Flames.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Flames : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Positions;
+    [SerializeField]
+    private ParticleSystem[] Flames;
+    [SerializeField]
+    private float DurationInSeconds = 10;
+    [SerializeField]
+    private bool FlamesTest = false;
+    [SerializeField]
+    private ParticleSystem FlamesPrefab;
+    
+    private Quaternion up;
+
+    void Start ()
+    {
+        up = Quaternion.Euler(0.0f, 90.0f, 0.0f);
+
+        Flames = new ParticleSystem[Positions.Length];
+
+        int i = 0;
+        foreach (Transform position in Positions)
+        {
+            Flames[i] = Instantiate(FlamesPrefab, position.position, up);
+            Flames[i].transform.SetParent(position);
+            Flames[i].transform.localRotation = up;
+            Flames[i].Pause();
+            i++;
+        }
+    }
+
+    private void Update()
+    {
+        if (FlamesTest)
+        {
+            StartCoroutine(_PlayFlames(DurationInSeconds));
+            FlamesTest = false;
+        }
+    }
+
+    public void FlamePlay(float duration)
+    {
+        StartCoroutine(_PlayFlames(duration));
+    }
+
+    private IEnumerator _PlayFlames(float duration)
+    {
+        foreach (ParticleSystem Flame in Flames)
+        {
+            if (!Flame.isPlaying)
+            {
+                Flame.Play(true);
+            }
+        }
+
+        yield return new WaitForSeconds(duration);
+
+        foreach (ParticleSystem Flame in Flames)
+        {
+            if (Flame.isPlaying)
+            {
+                Flame.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            }
+        }
+    }
+}
